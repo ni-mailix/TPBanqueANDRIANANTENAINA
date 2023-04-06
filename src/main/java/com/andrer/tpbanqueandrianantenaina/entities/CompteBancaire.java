987d7d5e17754c4a2fe8a30ef5f163e7ae10571c
André <ni.mailix@gmail.com>
2023-04-06 19:37:24 +0300

    06/04/2023 - 2ème commit 1:937

---
+++ src/main/java/com/andrer/tpbanqueandrianantenaina/entities/CompteBancaire.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.andrer.tpbanqueandrianantenaina.entities;
+
+import java.io.Serializable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ *
+ * @author Nirina
+ */
+@Entity
+public class CompteBancaire implements Serializable {
+    
+         private String nom;
+
+    /**
+     * Get the value of nom
+     *
+     * @return the value of nom
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @param nom new value of nom
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+        private int solde;
+
+    /**
+     * Get the value of solde
+     *
+     * @return the value of solde
+     */
+    public int getSolde() {
+        return solde;
+    }
+
+    /**
+     * Set the value of solde
+     *
+     * @param solde new value of solde
+     */
+    public void setSolde(int solde) {
+        this.solde = solde;
+    }
+
+     
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof CompteBancaire)) {
+            return false;
+        }
+        CompteBancaire other = (CompteBancaire) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.andrer.tpbanqueandrianantenaina.entities.CompteBancaire[ id=" + id + " ]";
+    }
+    
+   public CompteBancaire(String nom, int solde) {  
+  this.nom = nom;  
+  this.solde = solde;  
+}  
+                  
+public void deposer(int montant) {  
+  solde += montant;  
+}  
+                  
+public void retirer(int montant) {  
+  if (montant < solde) {  
+    solde -= montant;  
+  } else {
+    solde = 0;
+  }  
+}
+
+    public CompteBancaire(String nom, int solde, Long id) {
+        this.nom = nom;
+        this.solde = solde;
+        this.id = id;
+    }
+    
+}
